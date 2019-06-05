c2b72aab67d447b83922dfa4d8d70ed93376b821
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 17:39:56 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,39 +2,23 @@
 import React from 'react'
 
 class DigitalClicker extends React.Component {
-  
-   constructor() {
-    super();
-
-    this.state = {
-      errors: [],
-      user: null,
-      settings: {
-        bitrate: 8,
-        video: {
-          resolution: '1080p'
-        }
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
       }
-    };
+    }
   }
-
   
- handleChangeBitrate() {
-    this.setState({
-      settings: {...this.state.settings,
-      bitrate: 12}
-    });
-  };
-
-  handleChangeResolution = () => {
-    this.setState({
-      settings: {
-        ...this.state.settings,
-        video: {
-          ...this.state.settings.video,
+ this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
           resolution: '720p'
-        }
-      }
+        })
+      })
     });
   
   
