b35add7681a9ebc1d70e276992fb4574156ecef1
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 17:38:18 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  state = {
+    timesClicked: 0
+  }
+  
+  handleClick = () => {
+    this.setState(prev => ({
+      timesClicked: (prev.timesClicked + 1)
+    }))
+  }
+
+  
+  render(){
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      )
+  }
+  
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,34 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+  
+ this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
+          resolution: '720p'
+        })
+      })
+    });
+  
+  
+  render(){
+    return (
+      <div>
+        <button className='bitrate' onClick={this.handleChangeBitrate}>Change Bitrate</button>
+        <button className='resolution' onClick={this.handleChangeResolution}>Change Resolution</button>
+      </div>
+      )
+  }
+  
+}
